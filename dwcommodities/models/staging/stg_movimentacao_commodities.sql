07f9f5ef7a4d7f6c1d72c7bd59883e491fcd2e77
Breno Mendes <b.mendesbernardo2020@gmail.com>
2024-06-20 12:29:23 -0300

    feat: adicionar datamart e staging

---
+++ dwcommodities/models/staging/stg_movimentacao_commodities.sql
@@ -0,0 +1,23 @@
+
+-- models/staging/stg_movimentacao_commodities.sql
+
+with source as (
+    select
+        date,
+        symbol,
+        action,
+        quantity
+    from 
+        {{ source('dbcommoditiesdw', 'movimentacao_commodities') }}
+),
+
+renamed as (
+    select
+        cast(date as date) as data,
+        symbol as simbolo,
+        action as acao,
+        quantity as quantidade
+    from source
+)
+
+select * from renamed
